4071d7d1c4f228900359f7919b8b4f8852e3e0b4
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-29 06:02:47 +0530

    [abhishek] add: to create a address book table

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -1,3 +1,16 @@
 #uc1
 CREATE DATABASE address_book_service;
 USE address_book_service;
+
+#uc2
+CREATE TABLE address_book
+(
+firstName varchar (150) not null,
+lastName varchar (150) not null,
+address varchar (300) not null,
+city varchar (150) not null,
+state varchar (150) not null,
+zip double not null,
+phoneNumber double not null,
+email varchar (150) not null
+);
